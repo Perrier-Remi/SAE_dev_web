9e71715bb4e2fbce489148e767c3441ca8cd0f76
Yann Mijatovic <yann.mijatovic4@etu.univ-lorraine.fr>
2022-11-07 16:30:13 +0100

    Render Episode

--- src/classes/NetVOD/Episode.php
+++ src/classes/NetVOD/Episode.php
@@ -5,20 +5,26 @@ class Episode
     protected int $numero;
     protected string $titre;
     protected int $duree;
+    protected string $resume;
     protected string $cheminImage;
+    protected string $cheminFich;
 
     /**
      * @param int $numero
      * @param string $titre
      * @param int $duree
+     * @param string $resume
      * @param string $cheminImage
+     * @param string $cheminFich
      */
-    public function __construct(int $numero, string $titre, int $duree, string $cheminImage)
+    public function __construct(int $numero, string $titre, int $duree, string $resume, string $cheminImage, string $cheminFich)
     {
         $this->numero = $numero;
         $this->titre = $titre;
         $this->duree = $duree;
+        $this->resume = $resume;
         $this->cheminImage = $cheminImage;
+        $this->cheminFich = $cheminFich;
     }
 
 
