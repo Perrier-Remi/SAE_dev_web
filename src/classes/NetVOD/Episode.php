cfcff24dffdbf121954a032463f14bc6e8aae80f
Yann Mijatovic <yann.mijatovic4@etu.univ-lorraine.fr>
2022-11-07 15:04:44 +0100

    Modif Serie Episode

--- src/classes/NetVOD/Episode.php
+++ src/classes/NetVOD/Episode.php
@@ -6,4 +6,25 @@ class Episode
     protected string $titre;
     protected int $duree;
     protected string $cheminImage;
+
+    /**
+     * @param int $numero
+     * @param string $titre
+     * @param int $duree
+     * @param string $cheminImage
+     */
+    public function __construct(int $numero, string $titre, int $duree, string $cheminImage)
+    {
+        $this->numero = $numero;
+        $this->titre = $titre;
+        $this->duree = $duree;
+        $this->cheminImage = $cheminImage;
+    }
+
+
+    public function __get($property) {
+        if (property_exists($this, $property)) {
+            return $this->$property;
+        }
+    }
 }
--- src/classes/NetVOD/Serie.php
+++ src/classes/NetVOD/Serie.php
@@ -10,5 +10,31 @@ class Serie
     protected string $dateAjout;
     protected array $listeEpisode;
 
+    /**
+     * @param string $titre
+     * @param string $genre
+     * @param string $public
+     * @param string $descriptif
+     * @param int $dateSortie
+     * @param string $dateAjout
+     * @param array $listeEpisode
+     */
+    public function __construct(string $titre, string $genre, string $public, string $descriptif, int $dateSortie, string $dateAjout, array $listeEpisode)
+    {
+        $this->titre = $titre;
+        $this->genre = $genre;
+        $this->public = $public;
+        $this->descriptif = $descriptif;
+        $this->dateSortie = $dateSortie;
+        $this->dateAjout = $dateAjout;
+        $this->listeEpisode = $listeEpisode;
+    }
+
+
+    public function __get($property) {
+        if (property_exists($this, $property)) {
+            return $this->$property;
+        }
+    }
 
 }
