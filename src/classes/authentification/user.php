db310f0a6d70169ae00ce1539bf7d8d442daedc5
dekersab3u <dekersab3u@etu.univ-lorraine.fr>
2022-11-07 14:52:03 +0100

    Modification classe user

--- src/classes/authentification/user.php
+++ src/classes/authentification/user.php
@@ -3,6 +3,21 @@
 class user
 {
     public string $email;
+    public int $id;
     public string $hpassword;
+
+    /**
+     * @param string $email
+     * @param int $id
+     * @param string $hpassword
+     */
+    public function __construct(string $email, int $id, string $hpassword)
+    {
+        $this->email = $email;
+        $this->id = $id;
+        $this->hpassword = $hpassword;
+    }
+
 }
 
+?>
