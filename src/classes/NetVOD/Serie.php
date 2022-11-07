e2ad8fe7c3ed16daaf57e99b58c754f1220e8ac6
NaelGuenfoudi <guenfmen@gmail.com>
2022-11-07 15:36:18 +0100

    render , renderer

--- src/classes/NetVOD/Serie.php
+++ src/classes/NetVOD/Serie.php
@@ -1,9 +1,10 @@
 <?php
-
+namespace iutnc\deefy\NetVOD;
 class Serie
 {
     protected string $titre;
     protected string $genre;
+    protected string $cheminImage;
     protected string $public;
     protected string $descriptif;
     protected int $dateSortie;
@@ -31,10 +32,44 @@ class Serie
     }
 
 
-    public function __get($property) {
-        if (property_exists($this, $property)) {
-            return $this->$property;
-        }
+    public function __get(string $name)
+    {
+        if (property_exists($this, $name)) return $this->$name;
+        throw new InvalidPropertyNameException("Le nom est invalide");
     }
+
+    /**
+     * @throws InvalidPropertyNameException
+     * @throws NonEditablePropertyException
+     * @throws InvalidPropertyValueException
+     */
+    public function __set(string $name, $value): void
+    {
+        if(property_exists($this, $name)) {
+            if($name === "fichierAudio" || $name === "titre") throw new NonEditablePropertyException("$name ne peut etre modifier");
+            if($name === "duree" && $value < 0) throw new InvalidPropertyValueException("La duree ne peut etre negative");
+            $this->$name = $value;
+        } else throw new InvalidPropertyNameException("Le nom est invalide");
+    }
+
+    /** insert une serie dans la BDD
+     * @return void
+     */
+    public function insertSerie()
+    {
+        ConnectionFactory::setConfig("dbData.ini");
+        $db = ConnectionFactory::makeConnection();
+        $query = $db->prepare("insert into serie(titre, genre, descriptif,image, annee,date_ajout) values (?, ?,?, ?, ?, ?)");
+        $query->bindParam(1,$this->titre);
+        $query->bindParam(2, $this->genre);
+        $query->bindParam(3,$this->duree);
+        $query->bindParam(4,$this->fichierAudio);
+        $query->bindParam(5,$this->auteur);
+        $query->execute();
+        $query->closeCursor();
+        $db->commit();
+    }
+
+
 
 }
--- src/classes/render/RenderSerie.php
+++ src/classes/render/RenderSerie.php
@@ -1,13 +1,32 @@
 <?php
-
+namespace iutnc\deefy\render;
 namespace iutnc\netvod\render;
+
+use iutnc\deefy\NetVOD\Serie;
 
 class RenderSerie implements Renderer
 {
-
-    public function render(): string
+    protected Serie $serie;
+    public function __construct(Serie $serie)
     {
+        $this->serie=$serie;
+    }
+
+    public function render($selector=0): string
+    {   switch($selector){
+        case 0:
+            $this->renderCompact();
+    }
         $html = "";
         return $html;
+    }
+
+    private function renderCompact():string
+    {
+        $html="<img src=".$this->serie->;
+
+
+        return $html;
+
     }
 }
