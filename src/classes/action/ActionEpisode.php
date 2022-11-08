aaceb0f148f4b6a2d1c27c529532a4d42751796f
Yann Mijatovic <yannmijatovicp@gmail.com>
2022-11-08 14:55:25 +0100

    ActionEpisode

--- src/classes/action/ActionEpisode.php
+++ src/classes/action/ActionEpisode.php
@@ -2,7 +2,33 @@
 
 namespace iutnc\netvod\action;
 
-class ActionEpisode
+require_once 'src/classes/NetVOD/Episode.php';
+require_once 'src/classes/bd/ConnectionFactory.php';
+use iutnc\netvod\bd\ConnectionFactory;
+use iutnc\netvod\NetVOD\Episode;
+use iutnc\netvod\render\RenderEpisode;
+
+class ActionEpisode extends Action
 {
 
+    public function execute(): string
+    {
+        $html = "";
+        if (!isset($_GET['id_episode'])) {
+            $html .= "<p> id episode manquant</p>";
+        } else {
+            try {
+                $db = ConnectionFactory::makeConnection();
+            } catch (\Exception $e) {
+                $html .= "<p> Connection à la base de données impossible</p>";
+            }
+            $stmt = $db->prepare("SELECT * FROM episode WHERE id = ?");
+            $stmt->execute([$_GET['id_episode']]);
+            $donnees = $stmt->fetch();
+            $episode = new Episode($donnees[1], $donnees[2] , $donnees[4], $donnees[3], $donnees[5]);
+            $renderer = new RenderEpisode($episode);
+            $html .= $renderer->render();
+        }
+        return $html;
+    }
 }
