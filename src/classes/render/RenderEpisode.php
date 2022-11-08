89979e92a02ee9d40cd6d77302c65c7be2b767cf
Yann Mijatovic <yannmijatovicp@gmail.com>
2022-11-08 15:23:15 +0100

    Correction render episode

--- src/classes/render/RenderEpisode.php
+++ src/classes/render/RenderEpisode.php
@@ -24,8 +24,8 @@ class RenderEpisode implements Renderer
         } else {
             $html = "<div style=\"text-align:center\">". $this->episode->__get('titre')." </div>
              <div style=\"text-align:center\">". $this->episode->__get('resume')." </div>
-             <div style=\"text-align:center\">". $this->episode->__get('titre')." secondes </div>
-             <div style=\"text-align:center\"> <video controls width='600'> <source src='". $this->episode->__get('cheminFich')."'> </video> </div>";
+             <div style=\"text-align:center\">". $this->episode->__get('duree')." secondes </div>
+             <div style=\"text-align:center\"> <video controls width='600'> <source src='video/". $this->episode->__get('cheminFich')."'> </video> </div>";
         }
         return $html;
     }
