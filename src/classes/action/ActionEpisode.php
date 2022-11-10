364ebafc049c2e8dbfeef5e9f4100212b992b0b3
Yann Mijatovic <yannmijatovicp@gmail.com>
2022-11-10 16:45:18 +0100

    Ajout fonction 16

--- src/classes/action/ActionEpisode.php
+++ src/classes/action/ActionEpisode.php
@@ -35,13 +35,9 @@ class ActionEpisode extends Action
             }
             $id_serie = $serie_stmt->fetch()[0];
             $id_user = $_SESSION['id_user'];
-            $stmt_encours = $db->prepare("INSERT INTO serieEnCours(id_user, id_serie) VALUES (?,?)");
-            try {
-                $stmt_encours->execute([$id_user, $id_serie]);
-                $html .= "<div style=\"text-align:center\"><h3> Nouvelle série </h3> </div>";
-            } catch (\Exception $e) {
-                $html .= "<div style=\"text-align:center\"><h3> Série en cours </h3> </div>";
-            }
+
+            $html .= $this->serieEnCours($db, $id_user, $id_serie);
+            $html .= $this->episodeEnCours($db , $id_user, $id_serie);
 
 
             $stmt = $db->prepare("SELECT * FROM episode WHERE id = ?");
@@ -105,4 +101,91 @@ class ActionEpisode extends Action
                         <button type=\"submit\" name=\"commentaire\" value=\"vrai\"> Valider </button> </div>
                         </form>";
     } // changer notation
+
+    public function serieEnCours(\PDO $db, $id_user, $id_serie) : string {
+        $html = "";
+        $stmt_encours = $db->prepare("INSERT INTO serieEnCours(id_user, id_serie) VALUES (?,?)");
+        if (!$this->serieDejaVu($db, $id_user, $id_serie)) {
+            try {
+                $stmt_encours->execute([$id_user, $id_serie]);
+                $html .= "<div style=\"text-align:center\"><h3> Nouvelle série </h3> </div>";
+            } catch (\Exception $e) {
+                $html .= "<div style=\"text-align:center\"><h3> Série en cours </h3> </div>";
+            }
+        } else {
+            $html .= "<div style=\"text-align:center\"><h3> Série finie ! </h3> </div>";
+        }
+        return $html;
+    }
+
+    public function episodeEnCours(\PDO $db, $id_user, $id_serie) : string {
+        $html = "";
+        $stmt_episodeInListe = $db->prepare("SELECT COUNT(*) FROM episodeEnCours WHERE id_episode = ?");
+        $stmt_episodeInListe->execute([$_GET['id_episode']]);
+        // Si l'episode n'a jamais été regardé
+        if ($stmt_episodeInListe->fetch()[0] === 0) {
+            $stmt_episode = $db->prepare("INSERT INTO episodeEnCours(id_user, id_serie, id_episode, actuel) VALUES (?,?,?,true)");
+            try {
+                $stmt_episode->execute([$id_user, $id_serie, $_GET['id_episode']]);
+                $html .= "<div style=\"text-align:center\"><h3> Nouvel épisode </h3> </div>";
+            } catch (\Exception $e) {
+                $html .= "<div style=\"text-align:center\"><h3> Erreur dans la requête SQL epEnCours </h3> </div>";
+            }
+        // Si il a déjà été regardé
+        } else {
+            $stmt_episode = $db->prepare("UPDATE episodeEnCours SET actuel = true WHERE id_episode = ?");
+            try {
+                $stmt_episode->execute([$_GET['id_episode']]);
+                $html .= "<div style=\"text-align:center\"><h3> Revisionnage de l'épisode </h3> </div>";
+            } catch (\Exception $e) {
+                $html .= "<div style=\"text-align:center\"><h3> Erreur dans la requête SQL epEnCours </h3> </div>";
+            }
+        }
+
+        // Enlever l'ancien épisode courant si il existe
+        $stmt_oldEp = $db->prepare("SELECT id_episode FROM episodeEnCours WHERE actuel = true AND id_episode != ?");
+        try {
+            $stmt_oldEp->execute([$_GET['id_episode']]);
+            if (isset($stmt_oldEp->fetch(\PDO::FETCH_ASSOC)[0])) {
+                $oldEp = $stmt_oldEp->fetch()[0];
+                $stmt_updateOldEp = $db->prepare("UPDATE episodeEnCours SET actuel = false WHERE id_episode = ?");
+                $stmt_updateOldEp->execute([$oldEp]);
+            }
+        } catch (\Exception $e) {
+            $html .= "<div style=\"text-align:center\"><h3> Erreur dans la requête SQL epEnCours</h3> </div>";
+        }
+        return $html;
+    }
+
+    public function serieDejaVu(\PDO $db, $id_user, $id_serie) : bool {
+        $serieCompletee = false;
+        // Nombre total d'épisodes de la série
+        $stmt_nbEpSerie = $db->prepare("SELECT COUNT(*) FROM episode WHERE serie_id = ?");
+        $stmt_nbEpSerie->execute([$id_serie]);
+        $nbEpSerie = $stmt_nbEpSerie->fetch()[0];
+        // Nombre d'épisodes de la série déjà vus
+        $stmt_nbEpVus = $db->prepare("SELECT COUNT(*) FROM episodeEnCours WHERE id_serie = ?");
+        $stmt_nbEpVus->execute([$id_serie]);
+        $nbEpVus = $stmt_nbEpVus->fetch()[0];
+
+        // Si la série est complétée et tous les épisodes visionnés, la mettre dans la table serieDejaVisionnee
+        // Vérifier si la série est déjà comptée comme complétée
+        $stmt_verify = $db->prepare("SELECT COUNT(*) FROM serieDejaVisionnee WHERE id_user = ? AND id_serie = ?");
+        $stmt_verify->execute([$id_user, $id_serie]);
+        $serieIn = $stmt_verify->fetch()[0];
+
+        if ($serieIn === 0) {
+            if ($nbEpSerie === $nbEpVus) {
+                $stmt_add = $db->prepare("INSERT INTO serieDejaVisionnee(id_user, id_serie) VALUES (?,?)");
+                $stmt_delete = $db->prepare("DELETE FROM serieEnCours WHERE id_user = ? AND id_serie = ?");
+                $stmt_add->execute([$id_user, $id_serie]);
+                $stmt_delete->execute([$id_user, $id_serie]);
+                $serieCompletee = true;
+            }
+        } else {
+            $serieCompletee = true;
+        }
+
+        return $serieCompletee;
+    }
 }
